78000bf865d3b3b7988de552526227ca15ae9282
Yves Vinckier <yves.vinckier@live.fr>
2024-01-16 14:06:34 +0100

    about animation

--- src/components/general/NavAbout.js
+++ src/components/general/NavAbout.js
@@ -1,59 +1,107 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import { Link } from "gatsby";
 import styled from "@emotion/styled";
+
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
+gsap.registerPlugin(SplitText);
 
 const StyledNavAbout = styled.div`
   position: relative;
   mix-blend-mode: multiply;
-  color: white;
-  overflow: hidden;
   a {
-    display: block;
-    font-size: 1.2rem;
-    color: white;
+    position: relative;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    justify-content: center;
+    flex-direction: column;
     text-decoration: none;
-    font-family: "Inter", sans-serif;
     text-transform: uppercase;
-    &::after {
-      display: block;
-      content: "";
-      height: 1px;
-      background-color: white;
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      transform: translateX(-101%);
-    }
-    &:hover {
-      color: white;
-    }
-    &:hover::after {
-      transition: transform 0.3s cubic-bezier(0.5, 0.7, 0.4, 1);
-      transform: translateX(0);
-      background-color: white;
-    }
-    &.animate-out::after {
-      transition: transform 0.3s cubic-bezier(0.5, 0.7, 0.4, 1);
-      transform: translateX(100%);
+    color: white;
+    span {
+      will-change: transform;
+      font-family: "Inter", sans-serif;
+      perspective: 1000px;
+      font-size: 1.2rem;
+      font-weight: 400;
     }
   }
 `;
 
 const NavAbout = () => {
   const aboutRef = useRef();
+  const originalRef = useRef();
+  const cloneRef = useRef();
 
-  useEffect(() => {
-    aboutRef.current.addEventListener("mouseleave", () => {
-      aboutRef.current.classList.add("animate-out");
-    });
-    aboutRef.current.ontransitionend = function () {
-      aboutRef.current.classList.remove("animate-out");
-    };
-  }, []);
+  const tl = useRef();
+
+  const { contextSafe } = useGSAP();
+
+  useGSAP(
+    () => {
+      gsap.set(cloneRef.current, { yPercent: -100 });
+      SplitText.create(originalRef.current, {
+        type: "chars",
+      });
+      SplitText.create(cloneRef.current, { type: "chars" });
+    },
+    { scope: aboutRef }
+  );
+
+  const handleMouseEnter = contextSafe(({ scope: aboutRef }) => {
+    tl.current = gsap
+      .timeline({
+        defaults: { duration: 0.5, ease: "power2", stagger: 0.025 },
+      })
+      .to(originalRef.current.children, {
+        y: "100%",
+        rotationX: -90,
+        opacity: 0,
+      })
+      .to(
+        cloneRef.current.children,
+        {
+          startAt: { y: "-100%", rotationX: 90, opacity: 0 },
+          y: "0%",
+          rotationX: 0,
+          opacity: 1,
+        },
+        0
+      );
+  });
+
+  const handleMouseLeave = contextSafe(({ scope: aboutRef }) => {
+    tl.current = gsap
+      .timeline({
+        defaults: { duration: 0.5, ease: "power2", stagger: 0.025 },
+      })
+      .to(cloneRef.current.children, {
+        y: "-100%",
+        rotationX: 90,
+        opacity: 0,
+      })
+      .to(
+        originalRef.current.children,
+        {
+          startAt: { y: "100%", rotationX: -90, opacity: 0 },
+          y: "0%",
+          rotationX: 0,
+          opacity: 1,
+        },
+        0
+      );
+  });
+
   return (
-    <StyledNavAbout>
+    <StyledNavAbout
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <Link ref={aboutRef} to="/">
-        About
+        <span ref={originalRef}>About</span>
+        <span ref={cloneRef}>About</span>
       </Link>
     </StyledNavAbout>
   );
--- src/components/general/Navigation.js
+++ src/components/general/Navigation.js
@@ -4,17 +4,16 @@ import styled from "@emotion/styled";
 import NavLogo from "../general/NavLogo";
 import NavAbout from "../general/NavAbout";
 
-const StyledNavigationContainer = styled.div`
+const StyledNavigationContainer = styled.nav`
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 99;
+  z-index: 9999;
   display: flex;
-  padding: 0 60px;
+  padding: 30px 60px;
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 100px;
 `;
 
 const Navigation = () => {
